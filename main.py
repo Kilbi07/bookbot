05d3e55a27122ba07bccd4e2784de3bcc319ce0b
Kilbi07 <140620327+Kilbi07@users.noreply.github.com>
2024-08-01 15:16:35 +0200

    add print report function

--- main.py
+++ main.py
@@ -16,14 +16,25 @@ def count_character_appearance(text):
             character_dict[char] += 1
     return character_dict
 
+def sort_dict_descending(my_dict):
+    my_dict = sorted(my_dict.items(), key=lambda x:x[1], reverse=True)
+    sorted_dict = dict(my_dict)
+    return sorted_dict
+
+def print_report(book_path):
+    report=""
+    text = get_text_from_book(book_path)
+    num_words = count_words(text)
+    character_count = count_character_appearance(text)
+    sorted_character_count = sort_dict_descending(character_count)
+    report += f"--- Begin report of {book_path} ---\n {num_words} words found in the document\n\n"
+    for k,v in sorted_character_count.items():
+        if k.isalpha():
+            report+=f"The '{k}' character was found {v} times\n"
+    report+="--- End report ---"
+    print(report)
 
 def main():
-    book_path ="books/frankenstein.txt"
-    text = get_text_from_book(book_path)
-    print(text)
-    num_words = count_words(text)
-    print(f"The Number of words in Frankenstein is: {num_words}")
-    letter_count = count_character_appearance(text)
-    print(f"Anzahl der einzlnen Buchstaben: {letter_count}")
+    print_report("books/frankenstein.txt")
 
 main()
