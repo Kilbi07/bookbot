3af1b788f009219d0b590ddcedea4fffcb30f0f4
Kilbi07 <140620327+Kilbi07@users.noreply.github.com>
2024-08-01 13:16:02 +0200

    add functionality to read a book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def get_text_from_book(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    book_path ="books/frankenstein.txt"
+    text = get_text_from_book(book_path)
+    print(text)
+
+main()
